fe416b4c6bd937bcc4dae71a7cf08f2c98d448c5
Dominik Lazarek <dominik.lazarek@gmail.com>
2012-12-15 09:46:07 +0100

    Added first sphere

--- js/monsterScare.js
+++ js/monsterScare.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
 
 	init();
-
+	animate();
 } ); 
 
 
@@ -10,6 +10,7 @@ var SCREEN_HEIGHT = window.innerHeight;
 var camera;
 var scene;
 var globeMesh;
+var renderer;
 
 var container, stats;
 
@@ -20,7 +21,55 @@ function init()
 
 	scene = new THREE.Scene();
 
-	
+	renderer = new THREE.WebGLRenderer( { clearColor: 0x000000, clearAlpha: 1 } );
+	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+	renderer.domElement.style.position = "relative";
+	container.appendChild( renderer.domElement );
+	renderer.autoClear = false;
+
+	camera = new THREE.PerspectiveCamera( 60, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 200 );
+	camera.position.z = 100;
+
+	scene.camera = camera;
+
+	globeMesh = new THREE.Mesh( new THREE.SphereGeometry( 40, 100, 100 ), new THREE.MeshBasicMaterial( { color:0xff0000, wireframe: false } ) );
+	globeMesh.position.x = 0;
+	globeMesh.position.y = 0;
+	globeMesh.position.z = 0;
+	scene.add( globeMesh );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+	//document.addEventListener( 'keydown', onKeyDown, false );
+
+}
 
 
+
+function onWindowResize( event ) 
+{
+	SCREEN_WIDTH = window.innerWidth;
+	SCREEN_HEIGHT = window.innerHeight;
+
+	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+
+	camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+	camera.updateProjectionMatrix();
+}
+
+
+function animate() 
+{
+	requestAnimationFrame( animate );
+	render();
+}
+
+
+function render() 
+{
+	var r = Date.now() * 0.0005;
+
+	renderer.clear();
+
+	renderer.setViewport( 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT );
+	renderer.render( scene, camera );
 }
