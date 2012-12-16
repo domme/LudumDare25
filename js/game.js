252a126f4536895ec30699e152a8edcc4d40a1d0
Shane Grüling <sgrueling@portalwelten.de>
2012-12-16 16:24:36 +0100

    Balancing

--- js/game.js
+++ js/game.js
@@ -248,7 +248,7 @@ var monsterNameSpace = (function(ns)
         {
             this.xp = 0;
             this.level++;
-            this.daily_fee+=2000;
+            this.daily_fee+=1000;
         }
         game.gui.drawHUD();
     };
